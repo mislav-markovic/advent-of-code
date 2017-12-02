3539ad2b0ad32d3db63878d13993b06c8372b3af
Mislav <mislav.markovic@fer.hr>
2017-12-02 13:50:40 +0100

    Solution for part1 of day1

--- inverse_captcha/src/main.rs
+++ inverse_captcha/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let input = read_input();
+
+    let vec: Vec<i32> = input.chars().map(|a| a.to_string().parse::<i32>().unwrap()).collect();
+    let sum = vec.iter().enumerate().filter(|a| matches_next(&vec, a.0)).fold(0, |sum, x| sum+x.1);
+    println!("{}", sum);
+
+}
+
+fn matches_next(vec: &Vec<i32>, n: usize) -> bool{
+    if n >= vec.len() {
+        false
+    } else if n == vec.len() - 1 {
+        vec[n] == vec[0]
+    } else {
+        vec[n] == vec[n+1]
+    }
+}
+
+fn read_input() -> String {
+    use std::io;
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    input.trim().to_string()
 }
