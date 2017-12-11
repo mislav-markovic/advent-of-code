b9c5ced581bbe05b02dcaa73d357dbe392c695b6
Mislav <mislav.markovic@fer.hr>
2017-12-11 15:19:47 +0100

    Day11 part1 solution

--- aoc-17/hex_ed/src/main.rs
+++ aoc-17/hex_ed/src/main.rs
@@ -1,3 +1,67 @@
+use std::fs::File;
+use std::path::PathBuf;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+const FILE_PATH: &str = "./input.txt";
+
+struct Hex {
+    x: i32,
+    y: i32,
+    z: i32
+}
+
+impl Hex {
+    fn new() -> Hex {
+        Hex{x: 0, y: 0, z: 0}
+    }
+
+    fn move_hex(&mut self, direction: &str) {
+        match direction {
+            "n" => {self.y += 1; self.z -= 1},
+            "s" => {self.z += 1; self.y -= 1},
+            "nw" => {self.y += 1; self.x -= 1},
+            "ne" => {self.x += 1; self.z -= 1},
+            "sw" => {self.z += 1; self.x -= 1},
+            "se" => {self.x += 1; self.y -= 1},
+            _ => (),
+        };
+    }
+
+    fn dist_to(&self, other: &Hex) -> i32{
+        let vec = vec!(self.x - other.x, self.y - other.y, self.z - other.z);
+        *vec.iter().max().unwrap()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_input();
+    let mut start = Hex::new();
+
+    for dir in input {
+        start.move_hex(&dir[..]);
+    }
+
+    println!("{}", start.dist_to(&Hex::new()));
+
+}
+
+
+fn read_input() -> Vec<String> {
+    let f = File::open(PathBuf::from(FILE_PATH)).expect("file not found");
+    let mut result: Vec<String> = Vec::new();
+    let reader = BufReader::new(f);
+
+    reader.lines().for_each(|l| result.extend(parse(l.unwrap().trim().to_string())));
+    result
+}
+
+fn parse(line: String) -> Vec<String> {
+    let temp = line.split(",");
+    let mut vec = Vec::new();
+
+    for st in temp {
+        vec.push(st.trim().to_string());
+    }
+    vec
 }
