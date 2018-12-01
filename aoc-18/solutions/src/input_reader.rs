4f05d64f42be43d5f6c214f8b8068af98de9c477
Mislav <mislav.markovic@fer.hr>
2018-12-01 13:56:30 +0100

    input reader

---
+++ aoc-18/solutions/src/input_reader.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn read_all(file_name: &str) -> String {
+    let mut f = open_file(file_name);
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    contents
+}
+
+pub fn read_all_lines(file_name: &str) -> Vec<String> {
+    let mut contents = read_all(file_name);
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        result.push(line.to_owned());
+    }
+    result
+}
+
+pub fn open_file(file_name: &str) -> File {
+    let relative_path = String::from("input/".to_owned() + file_name);
+    println!("Reading from: {}", relative_path);
+    let mut f = File::open(relative_path).expect("file not found");
+    f
+}
