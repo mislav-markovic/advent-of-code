3c971bf05e5452fed4a4222d9fe36fc57fb12896
Mislav <mislav.markovic@fer.hr>
2018-12-04 11:02:27 +0100

    Day4 part1 structs

--- aoc-18/solutions/src/day4.rs
+++ aoc-18/solutions/src/day4.rs
@@ -1,7 +1,31 @@
 use crate::input_reader;
+use std::collections::HashMap;
+
+type guard_id_t = u32;
+type minutes_t = HashMap<u32, bool>;
+type shifts_t = Vec<Shift>;
+
+struct Date {
+    year: u32,
+    month: u32,
+    day: u32
+}
+
+struct Guard{
+    id: guard_id_t,
+    shifts: shifts_t
+}
+
+struct Shift{
+    date: Date,
+    minutes_asleep: minutes_t
+}
 
 fn do_the_job(input_location: &str) {
-    let data = input_reader::read_all_lines(input_location);
+    let mut data = input_reader::read_all_lines(input_location);
+    data.sort();
+
+
 }
 
 pub fn day4() {
