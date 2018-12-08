fa4f1a4ceeec50d52b5f6706a23c1610043d362c
Mislav <mislav.markovic@fer.hr>
2018-12-08 22:09:38 +0100

    Day 8 part 2

--- aoc-18/solutions/src/day8.rs
+++ aoc-18/solutions/src/day8.rs
@@ -1,10 +1,5 @@
 use crate::input_reader;
 
-enum State {
-    Header,
-    Data(usize),
-}
-
 struct Node {
     data: Vec<usize>,
     children: Vec<Box<Node>>,
@@ -29,58 +24,90 @@ impl Node {
     fn add_data(&mut self, data: &[usize]) {
         self.data.extend_from_slice(data);
     }
+
+    fn data_sum(&self) -> usize {
+        self.data.iter().sum()
+    }
+
+    fn node_value(&self) -> usize {
+        if self.children.is_empty() {
+            self.data_sum()
+        } else {
+            let mut result = 0;
+            for node_ref in self.data.iter() {
+                result += self
+                    .children
+                    .get(*node_ref - 1)
+                    .unwrap_or(&Node::new_boxed())
+                    .node_value();
+            }
+            result
+        }
+    }
 }
 
-fn part1(input: &str) -> usize {
-    use self::State::*;
-    use std::collections::VecDeque;
+fn do_the_job(input: &str) -> (usize, usize) {
     let data = input_reader::read_all(input);
-    let mut parsed = data
+    let parsed = data
         .trim()
         .split_whitespace()
         .map(|s| s.trim().parse::<usize>().unwrap())
         .collect::<Vec<_>>();
 
-    let (result, _) = sum_data(&parsed);
-    result
+    let (result, _, root) = traverse_data(&parsed);
+    (result, root.node_value())
 }
 
 //(sum, # of numbers read)
-fn sum_data(data: &[usize]) -> (usize, usize) {
-    //println!("{:?}", data);
+fn traverse_data(data: &[usize]) -> (usize, usize, Box<Node>) {
     let children = data[0];
     let data_count = data[1];
+    let mut node = Node::new_boxed();
 
     if children == 0 {
-        (data.iter().skip(2).take(data_count).sum(), 2 + data_count)
+        node.add_data(
+            data.iter()
+                .skip(2)
+                .take(data_count)
+                .cloned()
+                .collect::<Vec<_>>()
+                .as_slice(),
+        );
+        (node.data_sum(), 2 + data_count, node)
     } else {
         let (mut sum, mut skip): (usize, usize) = (0, 2);
 
         for _ in 0..children {
-            let (partial, jump) = sum_data(&data[skip..]);
+            let (partial, jump, child) = traverse_data(&data[skip..]);
             skip += jump;
             sum += partial;
+            node.add_child(child);
         }
-        let mine = data.iter().skip(skip).take(data_count).sum::<usize>();
-        sum += mine;
-        skip += data_count;
-        (sum, skip)
-    }
-}
 
-fn part2(input: &str) {
-    let data = input_reader::read_all(input);
+        node.add_data(
+            data.iter()
+                .skip(skip)
+                .take(data_count)
+                .cloned()
+                .collect::<Vec<_>>()
+                .as_slice(),
+        );
+        sum += node.data_sum();
+        skip += data_count;
+        (sum, skip, node)
+    }
 }
 
 pub fn day8() {
     let input = String::from("day8");
+    let (part1, part2) = do_the_job(&input);
 
     println!("***Day Eight***");
     println!("\tReading from {}", input);
     println!("\t**Part One**");
-    println!("\t\tMetadata entries sum: {}", part1(&input));
+    println!("\t\tMetadata entries sum: {}", part1);
     println!("\t**Part Two**");
-    //println!("\t\tShortest reduction: {}", part2(&input));
+    println!("\t\tRoot node value: {}", part2);
 }
 
 #[cfg(test)]
