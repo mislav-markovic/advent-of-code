27e2ae75bee20446ecec84a23f658ea39c91f1da
Mislav <mislav.markovic@fer.hr>
2018-12-09 10:55:11 +0100

    Day 9 part 1

--- aoc-18/solutions/src/day9.rs
+++ aoc-18/solutions/src/day9.rs
@@ -1,7 +1,66 @@
 use crate::input_reader;
 
-fn part1(input: &str) {
-    let data = input_reader::read_all_lines(input);
+struct Circle {
+    marbles: Vec<usize>,
+    players: Vec<usize>,
+    last_marble: usize,
+    current_marble: usize,
+    player_count: usize,
+}
+
+impl Circle {
+    fn new(last_marble: usize, player_count: usize) -> Circle {
+        Circle {
+            marbles: Vec::with_capacity(last_marble),
+            players: vec![0; player_count],
+            last_marble,
+            current_marble: 0,
+            player_count,
+        }
+    }
+
+    fn play(&mut self, marble: usize, player: usize) {
+        if self.marbles.is_empty() {
+            self.marbles.push(marble);
+        } else {
+            let pos = (self.current_marble + 2) % self.marbles.len();
+            if marble % 23 == 0 {
+                let remove_index = if self.current_marble < 7 {
+                    self.marbles.len() - (7 - self.current_marble)
+                } else {
+                    (self.current_marble - 7) % self.marbles.len()
+                };
+                self.players[player] += marble;
+                let removed_val = self.marbles.remove(remove_index);
+                self.players[player] += removed_val;
+                self.current_marble = remove_index;
+            } else {
+                self.marbles.insert(pos, marble);
+                self.current_marble = pos;
+            }
+        }
+    }
+
+    fn play_all(&mut self) {
+        (0..self.last_marble)
+            .zip((0..self.player_count).cycle())
+            .for_each(|(m, p)| self.play(m, p));
+    }
+
+    fn winning_score(&self) -> usize {
+        *self.players.iter().max().unwrap_or(&0)
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let data = input_reader::read_all(input);
+    let arr = data.split_whitespace().collect::<Vec<_>>();
+    let players = arr[0].parse::<usize>().unwrap();
+    let marbles = arr[6].parse::<usize>().unwrap();
+
+    let mut game = Circle::new(marbles, players);
+    game.play_all();
+    game.winning_score()
 }
 
 fn part2(input: &str) {
@@ -14,7 +73,7 @@ pub fn day9() {
     println!("***Day Nine***");
     println!("\tReading from {}", input);
     println!("\t**Part One**");
-    //println!("\t\tOrder of steps: {}", part1(&input));
+    println!("\t\tWinning elfs score: {}", part1(&input));
     //println!("\t**Part Two**");
     //println!("\t\tTime needed with 5 workers: {}", part2(&input));
 }
