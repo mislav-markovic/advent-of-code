b9b5346517a26bedb38a9ad2a2423cc313342a90
Mislav <mislav.markovic95@gmail.com>
2022-12-18 13:01:41 +0100

    Day 2 input and new executor

---
+++ aoc-22/src/days/day2.rs
@@ -0,0 +1,12 @@
+use crate::day_exec::DayExecutor;
+
+pub struct Day2;
+impl DayExecutor for Day2 {
+    fn exec_part1(&self, input: String) -> Box<dyn std::fmt::Display> {
+        todo!()
+    }
+
+    fn exec_part2(&self, input: String) -> Box<dyn std::fmt::Display> {
+        todo!()
+    }
+}
