0629479dea54a49e06c3c5f3304a965a53faaed3
Mislav <mislav.markovic95@gmail.com>
2022-12-18 12:57:58 +0100

    Both parts of day 1 share input so part 2 was cleanedup

--- aoc-22/src/main.rs
+++ aoc-22/src/main.rs
@@ -5,12 +5,12 @@ pub mod days;
 
 fn main() {
     let input =
-        std::fs::read_to_string("./input/day1.part1.txt").expect("Failed to read input file");
+        std::fs::read_to_string("./input/day1.part1.part2.txt").expect("Failed to read input file");
 
     let exec = Day1 {};
-    let day1_part1_solution = exec.exec_part1(input.clone());
-    let day1_part2_solution = exec.exec_part2(input);
+    let part1_solution = exec.exec_part1(input.clone());
+    let part2_solution = exec.exec_part2(input);
 
-    println!("Part 1:\n\t{}", day1_part1_solution);
-    println!("Part 2:\n\t{}", day1_part2_solution);
+    println!("Part 1:\n\t{}", part1_solution);
+    println!("Part 2:\n\t{}", part2_solution);
 }
