ab06e4ccdd18f0f5449c80fcc86cd75b77453d13
Mislav <mislav.markovic95@gmail.com>
2022-12-21 22:49:40 +0100

    Day 11 setup

---
+++ aoc-22/src/days/day11.rs
@@ -0,0 +1,15 @@
+use crate::day_exec::DayExecutor;
+
+use std::{fmt::Display, str::FromStr};
+
+pub struct Day11;
+
+impl DayExecutor for Day11 {
+    fn exec_part1(&self, input: String) -> Box<dyn Display> {
+        todo!()
+    }
+
+    fn exec_part2(&self, input: String) -> Box<dyn Display> {
+        todo!()
+    }
+}
--- aoc-22/src/main.rs
+++ aoc-22/src/main.rs
@@ -1,8 +1,8 @@
 use crate::{
     day_exec::DayExecutor,
     days::{
-        day1::Day1, day10::Day10, day2::Day2, day3::Day3, day4::Day4, day5::Day5, day6::Day6,
-        day7::Day7, day8::Day8, day9::Day9,
+        day1::Day1, day10::Day10, day11::Day11, day2::Day2, day3::Day3, day4::Day4, day5::Day5,
+        day6::Day6, day7::Day7, day8::Day8, day9::Day9,
     },
 };
 
@@ -10,159 +10,13 @@ pub mod day_exec;
 pub mod days;
 
 fn main() {
-    let input = std::fs::read_to_string("./input/day10.part1.part2.txt")
+    let input = std::fs::read_to_string("./input/day11.part1.part2.txt")
         .expect("Failed to read input file");
 
-    let exec = Day10 {};
+    let exec = Day11 {};
     let part1_solution = exec.exec_part1(input.clone());
     let part2_solution = exec.exec_part2(input);
 
     println!("Part 1:\n\t{}", part1_solution);
     println!("Part 2:\n\t{}", part2_solution);
 }
-const EXAMPLE: &'static str = "addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop";
