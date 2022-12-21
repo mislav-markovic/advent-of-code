4d799df8f92d879645811e4b94d4214eec52a57a
Mislav <mislav.markovic95@gmail.com>
2022-12-21 15:36:12 +0100

    Day 10 setup

---
+++ aoc-22/src/days/day10.rs
@@ -0,0 +1,7 @@
+use crate::day_exec::DayExecutor;
+
+use std::{collections::HashSet, str::FromStr};
+
+pub struct Day10;
+
+impl DayExecutor for Day10 {}
--- aoc-22/src/main.rs
+++ aoc-22/src/main.rs
@@ -1,8 +1,8 @@
 use crate::{
     day_exec::DayExecutor,
     days::{
-        day1::Day1, day2::Day2, day3::Day3, day4::Day4, day5::Day5, day6::Day6, day7::Day7,
-        day8::Day8, day9::Day9,
+        day1::Day1, day10::Day10, day2::Day2, day3::Day3, day4::Day4, day5::Day5, day6::Day6,
+        day7::Day7, day8::Day8, day9::Day9,
     },
 };
 
@@ -10,10 +10,10 @@ pub mod day_exec;
 pub mod days;
 
 fn main() {
-    let input = std::fs::read_to_string("./input/day09.part1.part2.txt")
+    let input = std::fs::read_to_string("./input/day10.part1.part2.txt")
         .expect("Failed to read input file");
 
-    let exec = Day9 {};
+    let exec = Day10 {};
     let part1_solution = exec.exec_part1(input.clone());
     let part2_solution = exec.exec_part2(input);
 
