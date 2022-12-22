4b1f3bb46892357115ea3868b31a1139748abff8
Mislav <mislav.markovic95@gmail.com>
2022-12-22 14:25:16 +0100

    Day 11 part 2 solution

--- aoc-22/src/days/day11.rs
+++ aoc-22/src/days/day11.rs
@@ -14,12 +14,13 @@ impl DayExecutor for Day11 {
     }
 
     fn exec_part2(&self, input: String) -> Box<dyn Display> {
-        Box::new(format!("Monkey business score: TODO"))
+        Box::new(format!("Monkey business score: {}", solve_part2(&input)))
     }
 }
 
-fn solve_part1(input: &str) -> usize {
+fn solve_part1(input: &str) -> u128 {
     let mut group = get_monkey_group_from_input(input);
+    group.relief_adujstment = Box::new(|i| i / 3);
 
     for _ in 0..20 {
         group.play_round();
@@ -33,20 +34,61 @@ fn solve_part1(input: &str) -> usize {
     last * second_to_last
 }
 
+fn solve_part2(input: &str) -> u128 {
+    let mut group = get_monkey_group_from_input(input);
+    let monkey_divisors = group
+        .monkey_items
+        .values()
+        .map(|(m, _)| m.tests_by)
+        .collect::<Vec<_>>();
+    let adjustment_val = lcmm(&monkey_divisors);
+
+    group.relief_adujstment = Box::new(move |i| i % adjustment_val);
+
+    for _ in 0..10000 {
+        group.play_round();
+    }
+
+    let mut num_of_inspects = group.inspect_counter.values().collect::<Vec<_>>();
+    num_of_inspects.sort();
+    let last = num_of_inspects.pop().unwrap();
+    let second_to_last = num_of_inspects.pop().unwrap();
+
+    last * second_to_last
+}
+
+fn lcmm(args: &[u128]) -> u128 {
+    args.iter().cloned().reduce(|a, b| lcm(a, b)).unwrap()
+}
+
+fn lcm(a: u128, b: u128) -> u128 {
+    a / gcd(a, b) * b
+}
+
+fn gcd(mut a: u128, mut b: u128) -> u128 {
+    while b > 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
 fn get_monkey_group_from_input(input: &str) -> MonkeyGroup {
     input
         .parse::<MonkeyGroup>()
         .expect("Failed to parse monkey group")
 }
 
-type MonkeyId = usize;
+type MonkeyId = u128;
 #[derive(Debug)]
-struct Item(usize);
+struct Item(u128);
 struct Monkey {
-    operation: Box<dyn Fn(usize) -> usize>,
-    test: Box<dyn Fn(usize) -> bool>,
+    operation: Box<dyn Fn(u128) -> u128>,
+    test: Box<dyn Fn(u128) -> bool>,
     test_success_target: MonkeyId,
     test_failure_target: MonkeyId,
+    tests_by: u128,
 }
 
 impl Monkey {
@@ -55,10 +97,11 @@ impl Monkey {
         test: TestF,
         test_success_target: MonkeyId,
         test_failure_target: MonkeyId,
+        tests_by: u128,
     ) -> Self
     where
-        OpF: Fn(usize) -> usize + 'static,
-        TestF: Fn(usize) -> bool + 'static,
+        OpF: Fn(u128) -> u128 + 'static,
+        TestF: Fn(u128) -> bool + 'static,
     {
         let operation = Box::new(op);
         let test = Box::new(test);
@@ -68,12 +111,17 @@ impl Monkey {
             test,
             test_success_target,
             test_failure_target,
+            tests_by,
         }
     }
 
-    fn inspect(&self, mut item: Item) -> (Item, MonkeyId) {
+    fn inspect(
+        &self,
+        mut item: Item,
+        relief_adjustment: impl Fn(u128) -> u128,
+    ) -> (Item, MonkeyId) {
         item.0 = (self.operation)(item.0);
-        item.0 /= 3;
+        item.0 = (relief_adjustment)(item.0);
 
         let target = if (self.test)(item.0) {
             self.test_success_target
@@ -112,7 +160,7 @@ impl FromStr for MonkeyTriplet {
             .trim()
             .trim_start_matches("Starting items:")
             .split(',')
-            .map(|s| s.trim().parse::<usize>().map(|v| Item(v)))
+            .map(|s| s.trim().parse::<u128>().map(|v| Item(v)))
             .collect::<Result<VecDeque<_>, _>>()
             .map_err(|_| MonkeyParseError {})?;
 
@@ -126,7 +174,7 @@ impl FromStr for MonkeyTriplet {
         let op = op.parse::<Operand>().map_err(|_| MonkeyParseError)?;
         let rhs = rhs.parse::<Variable>().map_err(|_| MonkeyParseError)?;
 
-        let operation = move |item: usize| op.calc(lhs.get_val(item), rhs.get_val(item));
+        let operation = move |item: u128| op.calc(lhs.get_val(item), rhs.get_val(item));
 
         let test_divisible_by =
             test_str
@@ -135,11 +183,11 @@ impl FromStr for MonkeyTriplet {
                 .and_then(|(_, divider)| {
                     divider
                         .trim()
-                        .parse::<usize>()
+                        .parse::<u128>()
                         .map_err(|_| MonkeyParseError {})
                 })?;
 
-        let test = move |item: usize| (item % test_divisible_by) == 0;
+        let test = move |item: u128| (item % test_divisible_by) == 0;
 
         let success_target_id = success_target_str
             .trim()
@@ -155,7 +203,13 @@ impl FromStr for MonkeyTriplet {
             .ok_or(MonkeyParseError)
             .and_then(|id_str| id_str.parse::<MonkeyId>().map_err(|_| MonkeyParseError {}))?;
 
-        let monkey = Monkey::new(operation, test, success_target_id, failure_target_id);
+        let monkey = Monkey::new(
+            operation,
+            test,
+            success_target_id,
+            failure_target_id,
+            test_divisible_by,
+        );
         Ok(MonkeyTriplet(monkey_id, monkey, starting_items))
     }
 }
@@ -167,7 +221,7 @@ enum Operand {
 }
 
 impl Operand {
-    fn calc(&self, lhs: usize, rhs: usize) -> usize {
+    fn calc(&self, lhs: u128, rhs: u128) -> u128 {
         match self {
             Operand::Add => lhs + rhs,
             Operand::Mul => lhs * rhs,
@@ -190,12 +244,12 @@ impl FromStr for Operand {
 
 #[derive(Debug)]
 enum Variable {
-    Constant(usize),
+    Constant(u128),
     Old,
 }
 
 impl Variable {
-    fn get_val(&self, current_val: usize) -> usize {
+    fn get_val(&self, current_val: u128) -> u128 {
         match self {
             Variable::Constant(constant_val) => *constant_val,
             Variable::Old => current_val,
@@ -214,7 +268,7 @@ impl FromStr for Variable {
             Ok(Self::Old)
         } else {
             trimmed
-                .parse::<usize>()
+                .parse::<u128>()
                 .map(|v| Self::Constant(v))
                 .map_err(|_| VariableParseError {})
         }
@@ -223,14 +277,16 @@ impl FromStr for Variable {
 
 struct MonkeyGroup {
     monkey_items: HashMap<MonkeyId, (Monkey, VecDeque<Item>)>,
-    inspect_counter: HashMap<MonkeyId, usize>,
+    inspect_counter: HashMap<MonkeyId, u128>,
+    relief_adujstment: Box<dyn Fn(u128) -> u128>,
 }
 
 impl MonkeyGroup {
-    fn new() -> Self {
+    fn new(relief_adujstment: impl Fn(u128) -> u128 + 'static) -> Self {
         Self {
             monkey_items: HashMap::new(),
             inspect_counter: HashMap::new(),
+            relief_adujstment: Box::new(relief_adujstment),
         }
     }
 
@@ -253,7 +309,7 @@ impl MonkeyGroup {
         let mut thrown_items: Vec<(Item, MonkeyId)> = Vec::with_capacity(items.len());
 
         while let Some(item) = items.pop_front() {
-            thrown_items.push(monkey.inspect(item));
+            thrown_items.push(monkey.inspect(item, &self.relief_adujstment));
 
             self.inspect_counter
                 .entry(*for_monkey)
@@ -275,7 +331,7 @@ impl FromStr for MonkeyGroup {
     type Err = MonkeyGroupParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut group = Self::new();
+        let mut group = Self::new(|i| i);
 
         s.split("\n\n")
             .map(|m| {
