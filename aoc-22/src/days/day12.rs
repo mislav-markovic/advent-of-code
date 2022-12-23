aa355a8f1b08ee4ddeb3b29416861161cf444bc9
Mislav <mislav.markovic95@gmail.com>
2022-12-23 14:51:50 +0100

    Day 12 part 1 WIP

--- aoc-22/src/days/day12.rs
+++ aoc-22/src/days/day12.rs
@@ -21,11 +21,20 @@ impl DayExecutor for Day12 {
 
 fn solve_part1(input: &str) -> u32 {
     let map = get_map_from_input(input);
-    let edge_weight = |_: &Position, _: &Position| -> u32 { 1 };
-    let heuristic = |current| manhattan_distance(current, &map.destination_pos);
-    let get_neighbours = |current| map.get_valid_neighbours(current);
+    let edge_weight = |_: Position, _: Position| -> u32 { 1 };
+    let goal = map.destination_pos.clone();
+    let heuristic = |current| manhattan_distance(&current, &goal);
+    let get_neighbours = |current: Position| map.get_valid_neighbours(&current);
 
-    todo!()
+    let res = a_star(
+        map.start_pos.clone(),
+        map.destination_pos.clone(),
+        get_neighbours,
+        heuristic,
+        edge_weight,
+    );
+
+    res.unwrap()
 }
 
 fn get_map_from_input(input: &str) -> Map {
@@ -79,7 +88,16 @@ impl Elevation {
     }
 }
 
-#[derive(Clone, PartialEq, Eq, Hash)]
+struct ElevationParseError;
+impl FromStr for Elevation {
+    type Err = ElevationParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Position {
     x: usize,
     y: usize,
@@ -134,18 +152,15 @@ impl Eq for Node {}
 fn a_star(
     start: Position,
     end: Position,
-    get_neighbours: impl Fn(&Position) -> Vec<Position>,
-    heuristic: impl Fn(&Position) -> u32,
-    edge_weight: impl Fn(&Position, &Position) -> u32,
+    get_neighbours: impl Fn(Position) -> Vec<Position>,
+    heuristic: impl Fn(Position) -> u32,
+    edge_weight: impl Fn(Position, Position) -> u32,
 ) -> Option<u32> {
     let mut open_set = BinaryHeap::new();
-    open_set.push(Reverse(Node::with_score(
-        start.clone(),
-        (heuristic)(&start),
-    )));
+    open_set.push(Reverse(Node::with_score(start, (heuristic)(start))));
 
     let mut goal_score = HashMap::new();
-    goal_score.insert(start.clone(), 0u32);
+    goal_score.insert(start, 0u32);
 
     while let Some(Reverse {
         0: Node { pos: current, .. },
@@ -155,15 +170,15 @@ fn a_star(
             return Some(goal_score[&current]);
         }
 
-        for neighbour in (get_neighbours)(&current) {
-            let tentative_score = goal_score[&current] + (edge_weight)(&current, &neighbour);
+        for neighbour in (get_neighbours)(current) {
+            let tentative_score = goal_score[&current] + (edge_weight)(current, neighbour);
 
-            let current_neighbour_score = goal_score.entry(neighbour.clone()).or_insert(Score::MAX);
+            let current_neighbour_score = goal_score.entry(neighbour).or_insert(Score::MAX);
             if tentative_score < *current_neighbour_score {
                 *current_neighbour_score = tentative_score;
 
                 if !open_set.iter().any(|n| n.0.pos == neighbour) {
-                    let f_score = tentative_score + (heuristic)(&neighbour);
+                    let f_score = tentative_score + (heuristic)(neighbour);
                     open_set.push(Reverse(Node::with_score(neighbour, f_score)))
                 }
             }
