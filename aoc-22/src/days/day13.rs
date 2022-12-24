938dd0eb6cc774039eab7e471c0b3bd70acdb5b7
Mislav Marković <mislav.markovic95@gmail.com>
2022-12-24 17:40:03 +0100

    Day 13 setup

---
+++ aoc-22/src/days/day13.rs
@@ -0,0 +1,14 @@
+use std::str::FromStr;
+
+use crate::day_exec::DayExecutor;
+
+pub struct Day13;
+impl DayExecutor for Day13 {
+    fn exec_part1(&self, input: String) -> Box<dyn std::fmt::Display> {
+        Box::new(format!("Part 1 TODO"))
+    }
+
+    fn exec_part2(&self, input: String) -> Box<dyn std::fmt::Display> {
+        Box::new(format!("Part 2 TODO"))
+    }
+}
--- aoc-22/src/main.rs
+++ aoc-22/src/main.rs
@@ -1,8 +1,8 @@
 use crate::{
     day_exec::DayExecutor,
     days::{
-        day1::Day1, day10::Day10, day11::Day11, day12::Day12, day2::Day2, day3::Day3, day4::Day4,
-        day5::Day5, day6::Day6, day7::Day7, day8::Day8, day9::Day9,
+        day1::Day1, day10::Day10, day11::Day11, day12::Day12, day13::Day13, day2::Day2, day3::Day3,
+        day4::Day4, day5::Day5, day6::Day6, day7::Day7, day8::Day8, day9::Day9,
     },
 };
 
@@ -10,10 +10,10 @@ pub mod day_exec;
 pub mod days;
 
 fn main() {
-    let input = std::fs::read_to_string("./input/day12.part1.part2.txt")
+    let input = std::fs::read_to_string("./input/day13.part1.part2.txt")
         .expect("Failed to read input file");
 
-    let exec = Day12 {};
+    let exec = Day13 {};
     let part1_solution = exec.exec_part1(input.clone());
     // let part1_solution = exec.exec_part1(EXAMPLE.to_owned());
 
@@ -24,8 +24,26 @@ fn main() {
     println!("Part 2:\n\t{}", part2_solution);
 }
 
-const EXAMPLE: &'static str = "Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi";
+const EXAMPLE: &'static str = "[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
