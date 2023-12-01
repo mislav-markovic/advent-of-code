9c529042954700549aecd52aa63ca17eb726f0ac
Mislav <mislav.markovic95@gmail.com>
2023-12-01 21:41:34 +0100

    AoC-23 Finish Day01 Part02

--- aoc-23/src/bin/day-01.rs
+++ aoc-23/src/bin/day-01.rs
@@ -1,7 +1,7 @@
 use phf::phf_map;
 
 const INPUT: &'static str = include_str!("input1.txt");
-const EXAMPLE: &'static str = r#"
+const _EXAMPLE: &'static str = r#"
 two1nine
 eightwothree
 abcone2threexyz
@@ -52,6 +52,7 @@ fn buffer_size() -> usize {
         .map(|w| w.len())
         .max()
         .expect("max size can be calculated")
+        + 1 // allow for extra chacater which causes us to detec we are no longer prefix
 }
 
 fn is_digit_match(candidate: &str) -> Option<u8> {
@@ -62,56 +63,104 @@ fn is_digit_prefix(candidate: &str) -> bool {
     DIGIT_WORDS.keys().any(|d| d.starts_with(candidate))
 }
 
-fn part2() {
+fn adjust_buffer(buf: &mut String) {
+    let mut skip = 0usize;
+    let mut should_clear = true;
+
+    while skip < buf.len() {
+        let maybe_prefix = &buf[skip..];
+
+        if is_digit_prefix(maybe_prefix) {
+            buf.replace_range(..skip, "");
+            should_clear = false;
+        }
+
+        skip += 1;
+    }
+
+    if should_clear {
+        buf.clear();
+    }
+}
+
+fn extract_val(line: &str) -> u32 {
+    let mut first_digit: Option<u8> = None;
+    let mut last_digit: Option<u8> = None;
     let mut buf = String::with_capacity(buffer_size());
-    let mut vals: Vec<u32> = Vec::new();
+
+    for c in line.chars() {
+        buf.push(c);
+        let digit = if c.is_numeric() {
+            buf.clear();
+
+            Some(c.to_digit(10).expect("we are working in base 10") as u8)
+        } else {
+            if let Some(d) = is_digit_match(&buf) {
+                buf.clear();
+                // is overlap like `oneight` -> on[e]ight -> 18 supported?
+                buf.push(c);
+                Some(d)
+            } else if !is_digit_prefix(&buf) {
+                println!("Adjusting buf from {buf}");
+                adjust_buffer(&mut buf);
+                println!("Adjusted buf to {buf}");
+                None
+            } else {
+                None
+            }
+        };
+
+        if let Some(d) = digit {
+            match (first_digit, last_digit) {
+                (None, _) => first_digit = Some(d),
+                _ => last_digit = Some(d),
+            }
+        }
+    }
+
+    let val = match (first_digit, last_digit) {
+        (Some(f), Some(l)) => (f * 10 + l).into(),
+        (Some(d), None) => (d * 10 + d).into(),
+        _ => {
+            eprintln!("WARN: No digits in line: {line}");
+            0
+        }
+    };
+
+    println!("INFO: {val} for: {line}");
+    val
+}
+
+fn part2() {
+    let mut sum = 0u32;
 
     let lines = INPUT.lines();
     for line in lines {
-        let mut first_digit: Option<u8> = None;
-        let mut last_digit: Option<u8> = None;
-        buf.clear();
-
-        for c in line.chars() {
-            buf.push(c);
-            let digit = if c.is_numeric() {
-                buf.clear();
-
-                Some(c.to_digit(10).expect("we are working in base 10") as u8)
-            } else {
-                if let Some(d) = is_digit_match(&buf) {
-                    buf.clear();
-                    Some(d)
-                    // assign
-                } else if !is_digit_prefix(&buf) {
-                    buf.clear();
-                    buf.push(c);
-                    None
-                } else {
-                    None
-                }
-            };
-
-            if let Some(d) = digit {
-                match (first_digit, last_digit) {
-                    (None, _) => first_digit = Some(d),
-                    _ => last_digit = Some(d),
-                }
-            }
-        }
-
-        match (first_digit, last_digit) {
-            (Some(f), Some(l)) => vals.push((f * 10 + l).into()),
-            (Some(d), None) => vals.push((d * 10 + d).into()),
-            _ => eprintln!("WARN: No digits in line: {line}"),
-        };
-        println!(
-            "INFO: {val} for: {line}",
-            val = vals.last().cloned().unwrap_or(0)
-        );
+        sum += extract_val(line);
     }
 
-    let rv: u32 = vals.iter().sum();
+    println!("Day-01 Part 02 Result is: {sum}");
+}
 
-    println!("Day-01 Part 02 Result is: {rv}");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_word_digits_detected() {
+        assert_eq!(is_digit_match("one"), Some(1));
+        assert_eq!(is_digit_match("two"), Some(2));
+        assert_eq!(is_digit_match("three"), Some(3));
+        assert_eq!(is_digit_match("four"), Some(4));
+        assert_eq!(is_digit_match("five"), Some(5));
+        assert_eq!(is_digit_match("six"), Some(6));
+        assert_eq!(is_digit_match("seven"), Some(7));
+        assert_eq!(is_digit_match("eight"), Some(8));
+        assert_eq!(is_digit_match("nine"), Some(9));
+    }
+
+    #[test]
+    fn extract_with_tricky_prefix() {
+        assert_eq!(extract_val("onine1"), 91);
+    }
 }
