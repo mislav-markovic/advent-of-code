7561eb740ebb5cea5a161cc73feb1f48531fdd15
Mislav <mislav.markovic95@gmail.com>
2023-12-02 23:52:22 +0100

    AoC-23 Day 3 setup

---
+++ aoc-23/src/bin/day-03.rs
@@ -0,0 +1,18 @@
+const INPUT: &'static str = include_str!("input3.txt");
+
+fn main() {}
+
+fn day3_exec() -> String {
+    let p1_res = part1();
+    let p2_res = part2();
+
+    format!("{p1_res}\n{p2_res}")
+}
+
+fn part1() -> String {
+    todo!()
+}
+
+fn part2() -> String {
+    todo!()
+}
